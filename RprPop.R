fcc94f47404509a845d9c7a8bb21f82fb32ee3c1
rougerbaptiste <rouger.b@gmail.com>
2015-05-04 10:28:20 +0200

    Ajout de commentaires explicatifs

--- RprPop.R
+++ RprPop.R
@@ -1,29 +1,30 @@
-rm(list=ls())
+rm(list=ls()) # on supprime tous les éléments de R
 
-donnees1 <- read.table("HauteursFinal.csv", header = T, sep=";")
+donnees1 <- read.table("HauteursFinal.csv", header = T, sep=";") # on charge mes données de hauteur
 donnees <- data.frame(donnees1)
 
-moyennes <- aggregate(donnees$hauteur,by=list(donnees$année,donnees$pop,donnees$lig),mean)
+moyennes <- aggregate(donnees$hauteur,by=list(donnees$année,donnees$pop,donnees$lig),mean) # on aggrège les données de hauteur en les moyennant pour l'année, la population et la lignée
 
 
+# on sélection chaque population de chaque lignée
 
 select1FPr <- moyennes[moyennes[,3]=="F252",]
 selectFPr <- select1FPr[moyennes[,2]=="PRECOCE",4]
-f252pre <- selectFPr[!is.na(selectFPr)]
+f252pre <- selectFPr[!is.na(selectFPr)] # contient les moyennes du précoce F252 pour chaque année
 
 select1FTe <- moyennes[moyennes[,3]=="F252",]
 selectFTe <- select1FTe[moyennes[,2]=="TEMOIN",4]
-f252tem <- selectFTe[!is.na(selectFTe)]
+f252tem <- selectFTe[!is.na(selectFTe)] # contient les moyennes du témoin F252
 
 select1FTa <- moyennes[moyennes[,3]=="F252",]
 selectFTa <- select1FTa[moyennes[,2]=="TARDIF",4]
-f252tar <- selectFTa[!is.na(selectFTa)]
+f252tar <- selectFTa[!is.na(selectFTa)] # contient les moyennes du tardif F252
 
 
 
 select1MPr <- moyennes[moyennes[,3]=="MBS",]
 selectMPr <- select1MPr[moyennes[,2]=="PRECOCE",4]
-mbspre <- selectMPr[!is.na(selectMPr)]
+mbspre <- selectMPr[!is.na(selectMPr)] # même chose avec MBS
 
 select1MTe <- moyennes[moyennes[,3]=="MBS",]
 selectMTe <- select1MTe[moyennes[,2]=="TEMOIN",4]
@@ -33,9 +34,11 @@ select1MTa <- moyennes[moyennes[,3]=="MBS",]
 selectMTa <- select1MTa[moyennes[,2]=="TARDIF",4]
 mbstar <- selectMTa[!is.na(selectMTa)]
 
+# l'abscisse du graphique est composée de toutes les années de 1998 à 2014 en enlevant 2011
 absc <- 1998:2014
 absc <- absc[-14]
-pdf("plot_pop-dist.pdf", onefile=T)
+
+pdf("plot_pop-dist.pdf", onefile=T) # on sort les graphique dans un pdf
 
 plot(f252pre ~ absc, type = "l" ,
 	ylim=c(90,180),
